c8e00baafb26b48b0e3eab7cf468f0e5191f27b9
aswinkp <kpaswin264@gmail.com>
2026-06-28 21:45:49 +0530

    Fix MongoDB connection

--- app.js
+++ app.js
@@ -60,11 +60,11 @@ db.connect((err) => {
     console.log("DB Connection Error:", err);
   } else {
     console.log("DB Connected Successfully");
+
+    app.use("/", userRouter);
+    app.use("/admin", adminRouter);
   }
 });
-
-app.use("/", userRouter);
-app.use("/admin", adminRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
