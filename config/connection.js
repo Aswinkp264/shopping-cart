06dcdcd4a3fa5a27e44155a4e5fb253f9156e309
aswinkp <kpaswin264@gmail.com>
2026-06-28 12:09:07 +0530

    added atlas connection and env config

--- config/connection.js
+++ config/connection.js
@@ -5,15 +5,16 @@ const state = {
 };
 
 module.exports.connect = async function (done) {
-  const url = "mongodb://127.0.0.1:27017";
-  const dbname = "shopping";
+  const url = process.env.MONGODB_URI || "mongodb://127.0.0.1:27017";
+  const dbname = process.env.DB_NAME || "shopping";
 
   try {
     const client = await MongoClient.connect(url);
     state.db = client.db(dbname);
-    // console.log("MongoDB Connected");
+    console.log("MongoDB Connected");
     done();
   } catch (err) {
+    console.log("MongoDB Connection Error:", err);
     done(err);
   }
 };
